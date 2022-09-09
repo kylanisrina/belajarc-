c9b2950aaa82e20c5130a15e54a126ebc831426f
KYLA NISRINA ANGGRAHINI <88182323+kylanisrina@users.noreply.github.com>
2022-09-09 15:37:47 +0700

    Add files via upload

---
+++ adp.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()}
+
+\begin{verbatim}
+sisa = 10 % 3; \\ nilainya 1
+bulat = 10 div 3;
+return 0;
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+//  1. Keseriusan
+//  3. Ber imajinasi
+//  
+//  kemampuan                                                        
